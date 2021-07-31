22cf29d20a9640a1e3039a0acffac733af179803
dyrstiu <vaatifelix@gmail.com>
2021-07-31 11:42:42 +0300

    a program to demonstrate literal constants

---
+++ literalconstants.java
@@ -0,0 +1,8 @@
+class Leteral {
+    public static void main(String []args){
+        System.out.print(3.142*7*7);
+        System.out.print("\n");
+        System.out.print("Full name \t score \t grade \n");
+        System.out.print("Kamau \t 82\t grade \t" +'A'+ "\n");
+    }
+}
